d7d913d5c92fb9c0dd630412eaa190d6670b96f8
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2023-01-06 22:57:59 +0100

    refactor: change actions

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import { runProxy as proxy } from './server/proxy';
-import fetch from './fetch';
+import axios from 'axios';
 import { load } from 'cheerio/lib/slim';
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs/promises';
 
-const link = 'http://extranet.ffta.fr/medias/documents_epreuves/';
-proxy().then(async () => {
-  const document = await fetch.fetchHTML(48000);
+const depCode = 48000;
+const url = `https://www.ffta.fr/ws/epreuves?ChxDepartement=${depCode}`;
+const baseMandatLink = 'http://extranet.ffta.fr/medias/documents_epreuves/';
+const year = new Date().getFullYear();
+
+(async () => {
+  const { data: document } = await axios.get<string>(url);
   const $ = load(document);
-
   const mandats = $('.results');
-  for (const mandat of mandats) {
-    const { href } = mandat.attribs;
-    const id = href.replace(link, '').replace('.pdf', '');
-    const filePath = path.join(__dirname, `/mandats/${id}.pdf`);
 
-    if (!fs.existsSync(filePath)) {
-      console.log('alert! new mandat found');
-      const response = await fetch.getMandat(id);
-      response.data.pipe(
-        fs.createWriteStream(path.join(__dirname, `/mandats/${id}.pdf`)),
-      );
-    }
+  const newDocuments = [];
+
+  for (const {
+    attribs: { href },
+  } of mandats) {
+    const id = href.replace(baseMandatLink, '').replace('.pdf', '');
+    newDocuments.push({ id, href });
   }
-});
+
+  await fs.writeFile(
+    `mandats-${year}.json`,
+    JSON.stringify(newDocuments, null, 2),
+  );
+})();
