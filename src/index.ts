30876c87d5c921283d680e19b875f93504eeb7be
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2023-01-10 14:42:27 +0100

    feat: change result to include name date and state

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,59 @@
 import fs from 'fs/promises';
 
 import axios from 'axios';
-import { load } from 'cheerio/lib/slim';
+import { load, Element } from 'cheerio/lib/slim';
 
 const depCode = 48000;
 const url = `https://www.ffta.fr/ws/epreuves?ChxDepartement=${depCode}`;
-const baseMandatLink = 'http://extranet.ffta.fr/medias/documents_epreuves/';
+
+function getInformationFromLine(line: Element) {
+  const dataChildren = line.children.filter(
+    (children) => children.type !== 'text',
+  );
+  const data = dataChildren[2] as Element;
+
+  // Warning: cannot be undefined
+  const dl = data.children.find(
+    (children) => children.type === 'tag',
+  ) as Element;
+
+  const result = dl.children.filter((children) => children.type === 'tag');
+
+  // @ts-expect-error miss types
+  const state = result[1].children[0].children[0].data;
+  let link;
+  if (state === 'Validée') {
+    // @ts-expect-error miss types
+    link = dataChildren[0].children
+      .filter((child: any) => child.type === 'tag')[1]
+      .children.filter((child: any) => child.type === 'tag')[0].attribs.href;
+  }
+
+  return {
+    state,
+    // @ts-expect-error miss types
+    org: format(result[3].children[0].data),
+    // @ts-expect-error miss types
+    name: format(result[5].children[0].data),
+    // @ts-expect-error miss types
+    date: format(result[17].children[0].data),
+    link,
+  };
+}
+
+function format(s: string): string {
+  return s.replace(/ {3,}/g, '').replaceAll('\n', '').replace('  ', ' ');
+}
 
 async function exec() {
   const { data: document } = await axios.get<string>(url);
   const $ = load(document);
-  const mandats = $('.results');
+  const mandats = $('.result-item');
 
   const newDocuments = [];
-
-  for (const {
-    attribs: { href },
-  } of mandats) {
-    const id = href.replace(baseMandatLink, '').replace('.pdf', '');
-    newDocuments.push({ id, href });
+  for (const line of mandats) {
+    const informations = getInformationFromLine(line);
+    newDocuments.push({ ...informations });
   }
 
   await fs.writeFile(
